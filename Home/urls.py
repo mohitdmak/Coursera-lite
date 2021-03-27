58669efb9dffae5078cdc9937e3c1c1f0bbe7c8c
mohitdmak <mdmohitrc@gmail.com>
2021-03-27 20:18:59 +0530

    Static files set to path and frontend home created

--- Home/urls.py
+++ Home/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views as Homeviews
 
 urlpatterns = [
     path('', Homeviews.home, name = 'home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
